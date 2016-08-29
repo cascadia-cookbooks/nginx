c18476fcfe8d5c08990209da2b6365671bca9540
Colby Olson <me@colbyolson.com>
2016-08-29 11:57:58 -0700

    Update metadata, remove lock file.

--- metadata.rb
+++ metadata.rb
@@ -2,13 +2,15 @@ name 'cop_nginx'
 maintainer 'Copious Inc.'
 maintainer_email 'engineering@copiousinc.com'
 license 'MIT'
-description 'Installs/Configures NGINX Web Server'
+description 'Installs and configures the NGINX web server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 chef_version '>= 12.5'
-version '0.0.2'
+version '0.1.0'
+
 source_url 'https://github.com/copious-cookbooks/nginx'
 issues_url 'https://github.com/copious-cookbooks/nginx/issues'
 
-supports 'ubuntu', '>= 14.04'
+supports 'ubuntu', '= 16.04'
+supports 'ubuntu', '= 14.04'
 
 depends 'apt'
