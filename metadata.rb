b66981d32f70cbc29f82c9ec5abea870940e66c6
Colby Olson <me@colbyolson.com>
2016-08-30 15:45:24 -0700

    removal

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,6 @@ maintainer_email 'engineering@copiousinc.com'
 license 'MIT'
 description 'Installs and configures the NGINX web server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-chef_version '>= 12.5'
 version '0.1.1'
 
 source_url 'https://github.com/copious-cookbooks/nginx'
