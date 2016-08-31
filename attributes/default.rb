56224c6ff412b2419e5f0ffa8fca84685cf1dce7
Colby Olson <me@colbyolson.com>
2016-08-30 17:28:18 -0700

    update attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,26 +1,22 @@
-# remove the default.conf webpage
-default['nginx']['remove_default_site'] = 'true'
-
-# install method
-default['nginx']['install_method'] = 'package'
+# master config options
+default['nginx'] = {
+    'remove_default_site' => 'true',
+    'install_method'      => 'package',
+    'user'                => 'www-data',
+    'worker_processes'    => 2,
+    'multi_accept'        => 'on',
+    'worker_connections'  => 1024,
+    'server_tokens'       => 'off',
+    'sendfile'            => 'on',
+    'tcp_nopush'          => 'on',
+    'tcp_nodelay'         => 'on',
+    'keepalive_timeout'   => 30,
+}
 
 # latest stable package release
 case lsb[:release]
 when '14.04'
-    default['nginx']['version'] = '1.4.6'
+    default['nginx']['version'] = '1.4.6-1ubuntu3.5'
 when '16.04'
-    default['nginx']['version'] = '1.10.0'
+    default['nginx']['version'] = '1.10.0-0ubuntu0.16.04.2'
 end
-
-# master config options
-default['nginx'] = {
-    'user'               => 'www-data',
-    'worker_processes'   => 2,
-    'multi_accept'       => 'on',
-    'worker_connections' => 1024,
-    'server_tokens'      => 'off',
-    'sendfile'           => 'on',
-    'tcp_nopush'         => 'on',
-    'tcp_nodelay'        => 'on',
-    'keepalive_timeout'  => 30,
-}
