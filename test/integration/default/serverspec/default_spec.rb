ccfc15afad6e97272528be1075473d7947a8515f
Colby Olson <me@colbyolson.com>
2016-09-13 18:00:54 -0700

    update tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,16 +1,10 @@
 require 'spec_helper'
 
 describe 'nginx::default' do
+  # TODO: test source recipe too
+  # TODO: move this to the package recipe
   describe package('nginx') do
     it { should be_installed }
-  end
-
-  describe command('nginx -v'), :if => os[:release] == '14.04' do
-    its(:stderr) { should match /1.4.6/ }
-  end
-
-  describe command('nginx -v'), :if => os[:release] == '16.04' do
-    its(:stderr) { should match /1.10.0/ }
   end
 
   it 'the NGINX service is enabled' do
@@ -21,12 +15,37 @@ describe 'nginx::default' do
     expect(service('nginx')).to be_running
   end
 
-  it 'creates a www-data user' do
-    expect(user('www-data')).to exist
-  end
+  case os[:release]
+  when '14.04'
+    describe command('nginx -v') do
+      its(:stderr) { should match /1.4.6/ }
+    end
 
-  it 'creates a www-data group' do
-    expect(group('www-data')).to exist
+  when '16.04'
+    describe command('nginx -v') do
+      its(:stderr) { should match /1.10.0/ }
+    end
+
+    describe user('www-data') do
+      it { should exist }
+    end
+
+    describe group('www-data') do
+      it { should exist }
+    end
+
+  when /7.2.*/
+    describe command('nginx -v') do
+      its(:stderr) { should match /1.10.1/ }
+    end
+
+    describe user('nginx') do
+      it { should exist }
+    end
+
+    describe group('nginx') do
+      it { should exist }
+    end
   end
 
   describe file('/etc/nginx/nginx.conf') do
