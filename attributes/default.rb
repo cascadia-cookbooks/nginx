13e74399bb9cfd43a6e7fe38163bffc6899f16fc
Colby Olson <me@colbyolson.com>
2017-04-05 11:54:29 -0700

    create attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,7 +10,26 @@ default['nginx'] = {
     'tcp_nopush'          => 'on',
     'tcp_nodelay'         => 'on',
     'keepalive_timeout'   => 30,
-    'vhost_dir'           => '/etc/nginx/conf.d'
+    'vhost_dir'           => '/etc/nginx/conf.d',
+    'gzip'                => 'on',
+    'gzip_vary'           => 'on',
+    'gzip_proxied'        => 'any',
+    'gzip_comp_level'     => 6,
+    'gzip_min_length'     => 1100,
+    'gzip_buffers'        => '16 8k',
+    'gzip_types'          => %w{
+        text/plain
+        text/css
+        text/js
+        text/xml
+        text/javascript
+        application/javascript
+        application/x-javascript
+        application/json
+        application/xml
+        application/xml+rss
+        image/svg+xml
+    }
 }
 
 case node['platform_family']
