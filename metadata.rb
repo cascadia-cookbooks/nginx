898d1c9e74196dc13e189899aa2075e3594dd346
Colby Olson <me@colbyolson.com>
2017-06-07 15:00:57 -0700

    bump to 0.5.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@copiousinc.com'
 license 'MIT'
 description 'Installs and configures the NGINX web server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.4.8'
+version '0.5.0'
 
 source_url 'https://github.com/copious-cookbooks/nginx'
 issues_url 'https://github.com/copious-cookbooks/nginx/issues'
