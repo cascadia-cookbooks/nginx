254c2bce9219178a50aa0ade806c618f9db48805
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-06-12 09:32:52 -0700

    Refactor service level test

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -7,12 +7,9 @@ describe 'nginx::default' do
     it { should be_installed }
   end
 
-  it 'the NGINX service is enabled' do
-    expect(service('nginx')).to be_enabled
-  end
-
-  it 'the NGINX service is running' do
-    expect(service('nginx')).to be_running
+  describe service('nginx') do
+      it { should be_enabled }
+      it { should be_running }
   end
 
   describe command('nginx -v') do
