310c0893b8b822c97fcd9ef24d9b9c054e17eff5
Colby Olson <me@colbyolson.com>
2017-09-14 15:12:52 -0700

    sort attributes, add master configs list, remove modules from kitchen

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,38 +1,41 @@
 # master config options
 default['nginx'] = {
-    'client_max_body_size'=> '1M',
-    'remove_default_site' => false,
-    'user'                => 'nginx',
-    'worker_processes'    => 2,
-    'multi_accept'        => 'on',
-    'worker_connections'  => 1024,
-    'server_tokens'       => 'off',
-    'sendfile'            => 'on',
-    'tcp_nopush'          => 'on',
-    'tcp_nodelay'         => 'on',
-    'keepalive_timeout'   => 30,
-    'proxy_read_timeout'  => 60,
-    'vhost_dir'           => '/etc/nginx/conf.d',
     'block_dir'           => '/etc/nginx/block.d',
+    'client_max_body_size'=> '1M',
     'gzip'                => 'on',
-    'gzip_vary'           => 'on',
-    'gzip_proxied'        => 'any',
+    'gzip_buffers'        => '16 8k',
     'gzip_comp_level'     => 6,
     'gzip_min_length'     => 1100,
-    'gzip_buffers'        => '16 8k',
+    'gzip_proxied'        => 'any',
     'gzip_types'          => %w{
-        text/plain
-        text/css
-        text/js
-        text/xml
-        text/javascript
         application/javascript
-        application/x-javascript
         application/json
+        application/x-javascript
         application/xml
         application/xml+rss
         image/svg+xml
+        text/css
+        text/javascript
+        text/js
+        text/plain
+        text/xml
     },
+    'gzip_vary'           => 'on',
+    'keepalive_timeout'   => 30,
+    'master_configs'      => {
+        'charset' => 'utf-8'
+    },
+    'multi_accept'        => 'on',
+    'proxy_read_timeout'  => 60,
+    'remove_default_site' => false,
+    'sendfile'            => 'on',
+    'server_tokens'       => 'off',
+    'tcp_nodelay'         => 'on',
+    'tcp_nopush'          => 'on',
+    'user'                => 'nginx',
+    'vhost_dir'           => '/etc/nginx/conf.d',
+    'worker_connections'  => 1024,
+    'worker_processes'    => 2,
     'ssl' => {
         'ciphers' => %w{
             HIGH
